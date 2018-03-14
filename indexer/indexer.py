10da6e2a70a9b174203aa9710d4fcfa69afd0fae
Alex Benito <abenito@usal.es>
2018-03-14 20:36:01 +0100

    - WIP indexer script now indexes some of the attributes mentioned in #25

--- indexer/indexer.py
+++ indexer/indexer.py
@@ -1,12 +1,15 @@
+import os
+import fnmatch
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk
 from bs4 import BeautifulSoup
 import pymysql
 
+
 print("Connecting to ES...")
-es = Elasticsearch(hosts=[{"host":'elasticsearch'}]) # what should I put here?
+es = Elasticsearch(hosts=[{"host":'elasticsearch'}])
 if not es.ping():
-    raise ValueError("Connection failed")
+	raise ValueError("Connection failed")
 else:
 	print('Connected to ES')
 
@@ -17,7 +20,72 @@ if conn.open:
 else:
 	print('Connection to MySQL failed')
 
-# create index
-# print("Indexing Elasticsearch db... (please hold on)")
-# bulk(es, actions)
-# print("...done indexing :-)")
+if es.indices.exists(index='dboe'):
+	print('dboe index exists, deleting...')
+	if es.indices.delete(index='dboe'):
+		print('dboe index deleted, will reindex now.')
+
+body = {"settings" : {
+		"index": { "number_of_shards": 1,
+					"number_of_replicas": 0
+				}
+	}}
+
+es.indices.create( index='dboe', ignore=400, body=body )
+
+actions = []
+
+rootPath = './data'
+pattern = 'r*.xml' #WIP: Test only with entries starting with 'r' for the moment
+
+#Walk data dir extracting the different entries
+for root, dirs, files in os.walk(rootPath):
+	for filename in fnmatch.filter(files, pattern):
+		print(os.path.join(root, filename))
+		soup = BeautifulSoup(open(os.path.join(root, filename), "r", encoding="utf-8"), 'xml')
+		for entry in soup.find_all("entry"):
+			entryObj = {}
+			entryObj['main_lemma'] = str(entry.form.orth.string)
+			if len(entryObj['main_lemma']) == 0:
+				continue
+			entryObj['id'] = entry['xml:id']
+			#part of speech
+			entryObj['pos'] = str(entry.gramGrp.pos.string)
+			
+			if entry.sense:
+				entryObj['sense'] = entry.sense.text.replace('\n', '')
+
+			if entry.note:
+				entryObj['note'] = entry.note.text.replace('\n', '')
+
+			
+			questionnaire = entry.findAll(
+								"ref", {"type": "fragebogenNummer"})
+			if len(questionnaire) > 0:
+				entryObj['questionnaire'] = questionnaire[0].string
+
+			
+			source = entry.findAll(
+								"ref", {"type": "quelle"})
+			if len(source) > 0:
+				entryObj['source'] = source[0].string
+
+
+			revised_source = entry.findAll(
+								"ref", {"type": "quelleBearbeitet"})
+			if len(revised_source) > 0:
+				entryObj['revised_source'] = revised_source[0].text
+
+
+			actions.append({
+					'_index': 'dboe',
+					'_type': 'dboe-type',
+					'_source': entryObj
+			})
+			if len(actions) > 50:
+				print('indexing 50 entries...')
+				bulk(es, actions)
+				print('indexed')
+				actions = []
+
+exit(0)
