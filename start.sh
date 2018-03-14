00c2054b255970d58b503a51854079e56240576a
Alex Benito <abenito@usal.es>
2018-03-14 20:33:07 +0100

    - start.sh script now requires TEI_DIR to be set to execute. - indexer container is kept running for dev purposes (indexer.py is run externally)

--- start.sh
+++ start.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
 # quick script to create and run docker image, with db migrations
+if [ -z ${TEI_DIR+x} ]; then 
+	echo "TEI_DIR variable is not set. Try setting it first using 'export TEI_DIR=/path/to/dir'";
+	exit;
+fi
+
+if [ ! -d "$TEI_DIR" ] || [ -z "$(ls -A $TEI_DIR)" ]; then
+	echo "$TEI_DIR does not exist or it is empty. Aborting.";
+	exit;
+fi
+
+echo "TEI_DIR is set to $TEI_DIR" 
 
 docker-compose up -d --build
+
 sleep 5
 #docker exec -it wugsy_web_1 python3 src/manage.py createsuperuser --name=admin --email=admin@admin.com --no-input
 docker exec -it wugsy_web_1 python3 src/manage.py makemigrations
